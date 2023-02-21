27bbe8a1d31286837b4281ff6f139ca5ba382fd1
ahmadkay <ahmadkamalabdallah@gmail.com>
2023-02-21 08:47:12 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Rerturn: Always 0 (Success)
+ * Return: Always 0 (sucess)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
