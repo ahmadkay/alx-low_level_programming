dd44a37353a293951357c9e4fe981a528a4ecaa0
ahmadkay <ahmadkamalabdallah@gmail.com>
2023-02-21 06:17:05 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %d bite(s)\n", sizeof(a));
+	printf("Size of an int; %d bite(s)\n", sizeof(b));
+	printf("Size of a long int; %d bite(s)\n", sizeof(c));
+	printf("Size of a long long int; %d bite(s)\n", sizeof(d));
+	printf("Size of a float; %d bite(s)\n", sizeof(e));
+	return (0);
+}
+
