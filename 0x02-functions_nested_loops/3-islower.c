02468edfb0de41c3ef6f307b75dc21afc455e5b0
ahmadkay <ahmadkamalabdallah@gmail.com>
2023-02-24 02:51:40 -0800

    is lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - check Holberton
+ * @j : an input character
+ * Description : fuction uses _putchar function to print 
+ * Return : 1 if j is lowercase and 0 if j is not lowercase
+ */
+
+int _islower(int c);
+{
+	if (islower(j))
+	{
+	return (1);
+	}
+	return (0);
+}
