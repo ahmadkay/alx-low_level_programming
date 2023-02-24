ff734ef5cc651c00c2c0ce17aaf04bfc41f09975
ahmadkay <ahmadkamalabdallah@gmail.com>
2023-02-24 02:02:08 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Check the code.
+ *
+ * Return : Always 0.
+ *
+ */
+
+int main(void)
+{
+	int _putchar(char c);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
