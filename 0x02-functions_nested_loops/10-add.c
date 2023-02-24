f587509ada77254f979f0b4b296bcc3aaa59e73a
ahmadkay <ahmadkamalabdallah@gmail.com>
2023-02-24 05:19:32 -0800

    addition of numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Check the code
+ *@a: integer input
+ *@b: integer input
+ * Description: This function prints the last digit of a number
+ * Return: the sum of a and b
+*/
+
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
