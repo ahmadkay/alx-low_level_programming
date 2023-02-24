ed5deeb5206bf9640436f971b378f47fb2d7c622
ahmadkay <ahmadkamalabdallah@gmail.com>
2023-02-24 05:51:35 -0800

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of natural numbers between 0 to 1024 excluding 1024
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int i, number;
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3) == 0 || (i % 5) == 0)
+{
+number = number + i;
+}
+}
+printf("%i\n", number);
+return (0);
