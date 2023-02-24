d7bc3a5615ad2bd2d2cbad16c615c163706584de
ahmadkay <ahmadkamalabdallah@gmail.com>
2023-02-24 03:09:52 -0800

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isalpha - Check the code
+ * @j: An input character
+ * Description: function uses _putchar function to print
+ * Return: 1 if j is an alphabet or 0 if j is not an alphabet
+ */
+
+int _isalpha(char j)
+{
+	if (isalpha(j))
+	{
+	return (1);
+	}
+	return (0);
+}
