9fad86ad3ad75e8f61319279cf26486ea6f41333
ahmadkay <ahmadkamalabdallah@gmail.com>
2023-02-24 03:21:50 -0800

    abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Check the code
+ * @n: An input interger
+ * Description: function uses _putchar function to print
+ * Return: returns 0.
+ */
+
+
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+	return (n);
+	}
+	return (n * -1);
+}
