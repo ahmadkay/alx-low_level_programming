20d70a771043f4649bff0a97d1ab319c0c2de98d
ahmadkay <ahmadkamalabdallah@gmail.com>
2023-02-24 20:06:51 -0800

    function loop

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - prints more numbers
+ * Return:0
+ */
+
+void more_numbers(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c += 1)
+	{
+		int i;
+
+		for (i = 0; i <= 14; i += 1)
+		{
+			if (i > 9)
+			{
+				_putchar ((i / 10) + '0');
+			}
+			_putchar ((i % 10) + '0');
+		}
+		_putchar ('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagonal - draw lines
+ * @n: no of dragonal
+ * Return:0
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar ('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i += 1)
+		{
+			for (j = 0; j < i; j += 1)
+			{
+				_putchar (32);
+			}
+			_putchar (92);
+			_putchar ('\n');
+		}
+	}
+}
