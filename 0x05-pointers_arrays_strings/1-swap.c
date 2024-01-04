d18fed1b9931885ac8e2e25157ab243aba0e6790
Ahmad Kamal <ahmadkamalabdallah@gmail.com>
2024-01-04 08:18:04 -0800

    swap 2 int

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - exchange the code
+ * @a: number to change
+ * @b: change the value
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a += *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
