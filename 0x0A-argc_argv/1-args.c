32c318028bb27dc00d064e5b4176215024278f03
Ahmad Kamal <ahmadkamalabdallah@gmail.com>
2024-01-12 13:37:25 -0800

    argc taks

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of argument
+ * @argc: number of arguments typed
+ * @argv: array pointing to arguements
+ * Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
